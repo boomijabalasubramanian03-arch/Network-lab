540d8ae780d7481ccbe5edefa17b754c6aaca6d2
Boomija Balasubramanian <boomijabalasubramanian03@gmail.com>
2026-09-22 00:00:12 +0530

    Implement UDP reverse server in ser2.c

---
+++ ex5/ser2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define SERVER_PORT 5001
+int s;
+int len;
+int i;
+char buffer[256];
+char revBuffer[256];
+struct sockaddr_in servAddr;
+struct sockaddr_in clntAddr;
+socklen_t clntAddrLen;
+
+int main(void)
+{
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(SERVER_PORT);
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    s = socket(PF_INET, SOCK_DGRAM, 0);
+    if (s < 0)
+    {
+        perror("Error: socket failed!");
+        exit(1);
+    }
+
+    if (bind(s, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror("Error: bind failed!");
+        exit(1);
+    }
+
+    printf("Reverse server started on port %d...\n", SERVER_PORT);
+
+        clntAddrLen = sizeof(clntAddr);
+
+        len = recvfrom(s, buffer, sizeof(buffer) - 1, 0,
+                        (struct sockaddr*)&clntAddr, &clntAddrLen);
+        if (len < 0)
+        {
+            perror("Error: recvfrom failed!");
+        }
+
+        buffer[len] = '\0';
+        printf("Received: %s\n", buffer);
+
+        for (i = 0; i < len; i++)
+            revBuffer[i] = buffer[len - 1 - i];
+        revBuffer[len] = '\0';
+
+        sendto(s, revBuffer, len, 0,
+               (struct sockaddr*)&clntAddr, clntAddrLen);
+
+    close(s);
+    return 0;
+}
